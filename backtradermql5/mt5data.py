472c22e8476b0c1ed4d43730dd7980a78eb2f213
Gunther Schulz <kunde2@guntherschulz.de>
2020-02-16 21:12:45 +0100

    add spread support for price candles

--- backtradermql5/mt5data.py
+++ backtradermql5/mt5data.py
@@ -1,5 +1,4 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from datetime import datetime
 
@@ -58,15 +57,20 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
 
       - `useask` (default: False)
         Use the ask price instead of the default bid price
+        
+      - `addspread` (default: False)
+        Add spread difference to candle price data.
 
     """
+
     params = (
-        ('historical', False),    # do backfilling at the start
-        ('backfill', True),       # do backfilling when reconnecting
-        ('backfill_from', None),  # additional data source to do backfill from
-        ('include_last', False),
-        ('reconnect', True),
-        ('useask', False)         # use the ask price instead of the default bid price
+        ("historical", False),  # do backfilling at the start
+        ("backfill", True),  # do backfilling when reconnecting
+        ("backfill_from", None),  # additional data source to do backfill from
+        ("include_last", False),
+        ("reconnect", True),
+        ("useask", False),  # use the ask price instead of the default
+        ("addspread", False),  # add spread difference to candle price data
     )
 
     _store = mt5store.MTraderStore
@@ -82,9 +86,9 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
     def __init__(self, **kwargs):
         self.o = self._store(**kwargs)
         # self._candleFormat = 'bidask' if self.p.bidask else 'midpoint'
-        #self.symbol = self.p.dataname
-        #self.timeframe = self.p.timeframe
-        #self.compression = self.p.compression
+        # self.symbol = self.p.dataname
+        # self.timeframe = self.p.timeframe
+        # self.compression = self.p.compression
 
     def setenvironment(self, env):
         """Receives an environment (cerebro) and passes it over to the store it
@@ -106,8 +110,7 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
         self.o.start(data=self)
 
         # Add server script symbol and time frame
-        self.o.config_server(
-            self.p.dataname, self.p.timeframe, self.p.compression)
+        self.o.config_server(self.p.dataname, self.p.timeframe, self.p.compression)
 
         # Backfill from external data feed
         if self.p.backfill_from is not None:
@@ -122,20 +125,24 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
     def _st_start(self):
         self.put_notification(self.DELAYED)
 
-        date_begin = num2date(
-            self.fromdate) if self.fromdate > float('-inf') else None
-        date_end = num2date(
-            self.todate) if self.todate < float('inf') else None
+        date_begin = num2date(self.fromdate) if self.fromdate > float("-inf") else None
+        date_end = num2date(self.todate) if self.todate < float("inf") else None
 
-        self.qhist = self.o.price_data(self.p.dataname, date_begin, date_end, self.p.timeframe,
-                                       self.p.compression, self.p.include_last)
+        self.qhist = self.o.price_data(
+            self.p.dataname,
+            date_begin,
+            date_end,
+            self.p.timeframe,
+            self.p.compression,
+            self.p.include_last,
+        )
 
         self._state = self._ST_HISTORBACK
 
         return True
 
     def stop(self):
-        '''Stops and tells the store to stop'''
+        """Stops and tells the store to stop"""
         super(MTraderData, self).stop()
         self.o.stop()
 
@@ -154,7 +161,7 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
                     return None
 
                 if msg:
-                    if msg['status'] == 'DISCONNECTED':
+                    if msg["status"] == "DISCONNECTED":
                         self.put_notification(self.DISCONNECTED)
 
                         if not self.p.backfill:
@@ -163,7 +170,7 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
                         self._statelivereconn = True
                         continue
 
-                    elif msg['status'] == 'CONNECTED' and self._statelivereconn:
+                    elif msg["status"] == "CONNECTED" and self._statelivereconn:
                         self.put_notification(self.CONNECTED)
                         self._statelivereconn = False
 
@@ -173,12 +180,16 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
                         self._st_start()
                         continue
 
-                    if msg['timeframe'] == self.o.get_granularity(self.p.timeframe, self.p.compression) and msg['symbol'] == self.p.dataname:
-                        if msg['timeframe'] == "TICK":
-                            if self._load_tick(msg['data']):
+                    if (
+                        msg["timeframe"]
+                        == self.o.get_granularity(self.p.timeframe, self.p.compression)
+                        and msg["symbol"] == self.p.dataname
+                    ):
+                        if msg["timeframe"] == "TICK":
+                            if self._load_tick(msg["data"]):
                                 return True  # loading worked
                         else:
-                            if self._load_candle(msg['data']):
+                            if self._load_candle(msg["data"]):
                                 return True  # loading worked
 
             elif self._state == self._ST_HISTORBACK:
@@ -231,8 +242,7 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
     def _load_tick(self, msg):
         time_stamp, _bid, _ask = msg
         # convert unix timestamp to float for millisecond resolution
-        d_time = datetime.utcfromtimestamp(
-            float(time_stamp) / 1000.0)
+        d_time = datetime.utcfromtimestamp(float(time_stamp) / 1000.0)
 
         dt = date2num(d_time)
 
@@ -254,10 +264,8 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
         return True
 
     def _load_candle(self, ohlcv):
-        # TODO support bid/ask using spread
-        time_stamp, _open, _high, _low, _close, _volume = ohlcv
-        d_time = datetime.utcfromtimestamp(
-            time_stamp)
+        time_stamp, _open, _high, _low, _close, _volume, _spread = ohlcv
+        d_time = datetime.utcfromtimestamp(time_stamp)
 
         dt = date2num(d_time)
 
@@ -265,11 +273,20 @@ class MTraderData(with_metaclass(MetaMTraderData, DataBase)):
         if dt <= self.lines.datetime[-1]:
             return False
 
+        def addspread(p, s):
+            return round(sum([float(p), float(-s)]), 5)
+
         self.lines.datetime[0] = dt
-        self.lines.open[0] = _open
-        self.lines.high[0] = _high
-        self.lines.low[0] = _low
-        self.lines.close[0] = _close
+        self.lines.open[0] = (
+            _open if not self.p.addspread else addspread(_open, _spread)
+        )
+        self.lines.high[0] = (
+            _high if not self.p.addspread else addspread(_high, _spread)
+        )
+        self.lines.low[0] = _low if not self.p.addspread else addspread(_low, _spread)
+        self.lines.close[0] = (
+            _close if not self.p.addspread else addspread(_close, _spread)
+        )
         self.lines.volume[0] = _volume
         self.lines.openinterest[0] = 0.0
         return True
