5649851ad7b52ced9582a62b62bc6f4b540f81b7
Gunther Schulz <dev@guntherschulz..de>
2020-10-31 11:34:16 +0100

    fix bug that prevented opening multiple charts
    Custom class attributes should only be declared during __init__

--- backtradermql5/mt5chart.py
+++ backtradermql5/mt5chart.py
@@ -7,52 +7,55 @@ class MTraderChart(bt.Indicator):
 
     # Inherited Indicator class requires at least one line
     lines = ("dummyline",)
-    params = (("data_obj", None),)
+    # params = (("data_obj", None),)
 
-    line_store = list()
-    indicator_buffers = dict()
-    subwindow_count = 0
-    # TODO implent drawing of chart objects
-    # graphic_types = ["curve", "line", "arrowbuy", "arrowsell"]
+    # Do not define any class attributes here. All MTraderChart would share the same attribute.
+    # This is probably because of the class inheritance of bt.indicator?
 
     def __init__(self):
         """
         Opens a chart window in MT5 withe the smybol and timeframe of the passed data stream object.
         """
+        self.line_store = list()
+        self.indicator_buffers = dict()
+        self.subwindow_count = 0
+        # TODO implent drawing of chart objects
+        # graphic_types = ["curve", "line", "arrowbuy", "arrowsell"]
         self.p.chartId = str(uuid.uuid4())
-        self.p.symbol = self.p.data_obj._name
-        self.p.timeframe = self.p.data_obj._timeframe
-        self.p.compression = self.p.data_obj._compression
-        self.p.store = self.p.data_obj.o
+        self.p.symbol = self.data._name
+        self.p.timeframe = self.data._timeframe
+        self.p.compression = self.data._compression
+        self.p.store = self.data.o
 
         self.p.store.config_chart(self.p.chartId, self.p.symbol, self.p.timeframe, self.p.compression)
 
     def next(self):
-        state = self.p.data_obj._state
-        qsize = self.p.data_obj._historyback_queue_size
-        _ST_LIVE = self.p.data_obj._ST_LIVE
+        state = self.data._state
+        qsize = self.data._historyback_queue_size
+        _ST_LIVE = self.data._ST_LIVE
 
         for obj in self.line_store:
-            # Wait for first indicator calculation
-            mp = obj["line"]._minperiod
+            # # Support for repainting indicators
+            # # Wait for first indicator calculation
+            # mp = obj["line"]._minperiod
+            # if obj["line"].lencount > 0:
 
-            if obj["line"].lencount > 0:
-                date = self.p.data_obj.datetime.datetime()
-                value = obj["line"][0]
-                if date != obj["last_date"] and not math.isnan(value):
-                    obj["values"].append(value)
-                    # Push historical indicator values when all historical price data has been processed
-                    if qsize <= 1 or state == _ST_LIVE:
-                        # Support for repainting indicators
-                        # if obj["repaint"] is True and obj["line"].lencount >= mp:
-                        #     obj["values"] = obj["values"][: len(obj["values"]) - mp]
-                        #     for i in reversed(range(0, mp)):
-                        #         obj["values"].append(obj["line"][i * -1])
-                        self.p.store.push_chart_data(
-                            self.p.chartId, obj["chartIndicatorId"], obj["bufferId"], obj["values"],
-                        )
-                        obj["values"] = []
-                    obj["last_date"] = date
+            date = self.data.datetime.datetime()
+            value = obj["line"][0]
+            if date != obj["last_date"] and not math.isnan(value):
+                obj["values"].append(value)
+                # Push historical indicator values when all historical price data has been processed
+                if qsize <= 1 or state == _ST_LIVE:
+                    # # Support for repainting indicators
+                    # if obj["repaint"] is True and obj["line"].lencount >= mp:
+                    #     obj["values"] = obj["values"][: len(obj["values"]) - mp]
+                    #     for i in reversed(range(0, mp)):
+                    #         obj["values"].append(obj["line"][i * -1])
+                    self.p.store.push_chart_data(
+                        self.p.chartId, obj["chartIndicatorId"], obj["bufferId"], obj["values"],
+                    )
+                    obj["values"] = []
+                obj["last_date"] = date
 
     def addindicator(self, window=0, shortname="JsonAPIIndiatorSH"):
         """
